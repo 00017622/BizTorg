e18b46cdb26aab1e0f58aea60fdd308e0deeaf2f
00017622 <00017622bs@gmail.com>
2025-07-28 17:23:39 +0500

    change

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['GET', 'POST', 'PUT', 'OPTIONS'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Origin', 'Content-Type', 'X-Auth-Token', 'Cookie'],
+    'allowed_headers' => ['Origin', 'Content-Type', 'X-Auth-Token', 'Cookie', 'Authorization'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
